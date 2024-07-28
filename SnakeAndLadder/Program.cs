e992f9b962815344878c10a616d07fc941aef311
arshedgithub <arshedahmedmail@gmail.com>
2024-07-28 19:38:56 +0530

    Add project files.

---
+++ SnakeAndLadder/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace snakeAndLadderGame
+{
+    class Program
+    {
+        static void Main(string[] args) 
+        {
+            Console.WriteLine("Hello World!");
+
+            Random rad = new Random();
+            int randomNum = rad.Next(1,7);
+            Console.WriteLine(randomNum);
+        }
+    }
+}
