b9dd9cc8d722ae210caaeeb39ee51d20202c516a
arshedgithub <arshedahmedmail@gmail.com>
2024-07-28 21:54:45 +0530

    check player position with ladder and snakes

--- SnakeAndLadder/Program.cs
+++ SnakeAndLadder/Program.cs
@@ -12,11 +12,10 @@ namespace snakeAndLadderGame
             { 8, 34 },
             { 20, 77 },
             { 32, 68 },
-            { 32, 68 },
-            { 41, 79},
-            { 74, 88},
-            { 85, 95},
-            { 82, 100}
+            { 41, 79 },  
+            { 74, 88 },
+            { 85, 95 },
+            { 82, 100 }
         };
 
         // snakes
@@ -32,13 +31,14 @@ namespace snakeAndLadderGame
             { 97, 25}
         };
 
-
-
-
         static void Main(string[] args) 
         {
             int player_1_position = 0;
             int player_2_position = 0;
+
+            Console.WriteLine("-------------------------------------------------------------------------------");
+            Console.WriteLine("                           Snake And Ladder Game");
+            Console.WriteLine("-------------------------------------------------------------------------------");
 
             Console.Write("Enter the player 1 name : ");
             string player_1 = Console.ReadLine();
@@ -59,15 +59,15 @@ namespace snakeAndLadderGame
                         int diceValue = rollingDice();
                         Console.WriteLine("Dice Value : " + diceValue);
                         player_1_position += diceValue;
+                        player_1_position = changePlayerPosition(player_1_position, player_1);
                         turnOfFirstPlayer = !turnOfFirstPlayer;
-                        Console.WriteLine(player_1 + " Your new position is " + player_1_position);
                     } else
                     {
                         Console.WriteLine("Press r for roll dice!");
                     }
                 } else
+                
                 // turn of second palyer
-
                 {
                     Console.Write($"{player_2}, Please roll the dice : ");
                     if (Console.ReadLine() == "r")
@@ -75,8 +75,8 @@ namespace snakeAndLadderGame
                         int diceValue = rollingDice();
                         Console.WriteLine("Dice Value : " + diceValue);
                         player_2_position += diceValue;
+                        player_2_position = changePlayerPosition(player_2_position, player_2);
                         turnOfFirstPlayer = !turnOfFirstPlayer;
-                        Console.WriteLine(player_2 + " Your new position is " + player_2_position);
                     }
                     else
                     {
@@ -91,6 +91,35 @@ namespace snakeAndLadderGame
         {
             Random random = new Random();
             return random.Next(1, 7);
+        }
+
+        // checking the position whether it was on a snake or ladder
+        static int changePlayerPosition(int position, string player)
+        {
+            int new_position;
+
+            if (ladders.ContainsKey(position))
+            {
+                new_position = ladders[position];
+                Console.WriteLine(player + ", You reach a ladder. Your new position is " + new_position);
+            }
+            else if (snakes.ContainsKey(position))
+            {
+                new_position = snakes[position];
+                Console.WriteLine(player + ", You stepped on a snake. Your new position is " + new_position);
+            } else
+            {
+                new_position = position;
+                Console.WriteLine(player + " Your new position is " + new_position);
+            }
+
+            if (new_position >= 100)
+            {
+                Console.WriteLine();
+                Console.WriteLine(player + " Wins!");
+            }
+
+            return new_position;
         }
     }
 }
