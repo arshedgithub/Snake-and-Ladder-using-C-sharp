3addca69368c9d65b45fab45f70bca96f80650b2
arshedgithub <arshedahmedmail@gmail.com>
2024-07-28 19:57:42 +0530

    add snakes and ladders

--- SnakeAndLadder/Program.cs
+++ SnakeAndLadder/Program.cs
@@ -4,13 +4,47 @@ namespace snakeAndLadderGame
 {
     class Program
     {
+
+        // ladders
+        static Dictionary<int, int> ladders = new Dictionary<int, int>()
+        {
+            { 2, 23 },
+            { 8, 34 },
+            { 20, 77 },
+            { 32, 68 },
+            { 32, 68 },
+            { 41, 79},
+            { 74, 88},
+            { 85, 95},
+            { 82, 100}
+        };
+
+        // snakes
+        static Dictionary<int, int> snakes = new Dictionary<int, int>()
+        {
+            { 29, 9 },
+            { 38, 15 },
+            { 47, 5 },
+            { 53, 33 },
+            { 62, 37 },
+            { 86, 54},
+            { 92, 70},
+            { 97, 25}
+        };
+
+
         static void Main(string[] args) 
         {
             Console.WriteLine("Hello World!");
+             
+            
+        }
 
-            Random rad = new Random();
-            int randomNum = rad.Next(1,7);
-            Console.WriteLine(randomNum);
+        // function for generating number between 1 to 6 using Random class
+        static int rollingDice()
+        {
+            Random random = new Random();
+            return random.Next(1, 7);
         }
     }
 }
